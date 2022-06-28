3cf3ac10aab13c44dc118c4ea747dcc33c2916f2
roberto <rdcg18@gmail.com>
2022-06-28 09:01:14 -0500

    280622

--- ejercicios/19_ConexionBD.php
+++ ejercicios/19_ConexionBD.php
@@ -30,3 +30,33 @@ try {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<table>
+    <thead>
+        <tr>
+            <th>Título</th>
+            <th>Fecha</th>
+        </tr> 
+    </thead>
+    <tbody>
+        <tr>
+            <?php foreach($resultado as $resultad){?>
+                <tr>
+                    <td>$resultado->archivo</td>
+                </tr>
+            <?php } ?>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
